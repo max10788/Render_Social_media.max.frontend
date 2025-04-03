0234176d02e840cce541154d7260e23494e6ca56
max10788 <98173596+max10788@users.noreply.github.com>
2025-04-03 09:11:01 +0200

    Update database.py

--- app/core/database.py
+++ app/core/database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from app.core.config import settings
-from app.models.db_models import Base  # Importieren Sie das Base-Objekt
 
 # Verbindung zur Datenbank herstellen
 DATABASE_URL = settings.DATABASE_URL
@@ -11,9 +10,9 @@ engine = create_engine(DATABASE_URL)
 # SessionLocal für die Datenbankverbindung
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+# Basis für die Datenbankmodelle
+Base = declarative_base()
+
 # Funktion zum Erstellen der Tabellen
 def init_db():
     Base.metadata.create_all(bind=engine)
-
-# Initialisieren der Datenbank beim Start der Anwendung
-init_db()
