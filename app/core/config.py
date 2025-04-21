ebe9c8b30c5a148fa2ebb82ae9cafe4b2d320c1a
max10788 <98173596+max10788@users.noreply.github.com>
2025-04-21 13:14:44 +0200

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,24 +1,20 @@
 from pydantic import BaseSettings
+from dotenv import load_dotenv
+import os
+
+# Lade die .env-Datei
+load_dotenv()
 
 class Settings(BaseSettings):
-    # Datenbank- und Redis-Konfiguration
-    DATABASE_URL: str
-    REDIS_URL: str
-
-    # Twitter-API-Konfiguration
-    TWITTER_BEARER_TOKEN: str
-    TWITTER_API_KEY: str
-    TWITTER_API_SECRET: str
-    TWITTER_ACCESS_TOKEN: str
-    TWITTER_ACCESS_SECRET: str
-
-    # Blockchain-spezifische Konfigurationen
-    SOLANA_RPC_URL: str  # URL für die Solana JSON RPC API
-    ETHEREUM_RPC_URL: str  # URL für die Ethereum JSON RPC API (z. B. Infura oder Alchemy)
-    MORALIS_API_KEY: str  # API-Schlüssel für Moralis
-    MORALIS_BASE_URL: str = "https://deep-index.moralis.io/api/v2"  # Basis-URL für Moralis
-
-    class Config:
-        env_file = ".env"  # Lädt die Umgebungsvariablen aus der .env-Datei
+    DATABASE_URL: str = os.getenv("DATABASE_URL")
+    REDIS_URL: str = os.getenv("REDIS_URL")
+    TWITTER_BEARER_TOKEN: str = os.getenv("TWITTER_BEARER_TOKEN")
+    TWITTER_API_KEY: str = os.getenv("TWITTER_API_KEY")
+    TWITTER_API_SECRET: str = os.getenv("TWITTER_API_SECRET")
+    TWITTER_ACCESS_TOKEN: str = os.getenv("TWITTER_ACCESS_TOKEN")
+    TWITTER_ACCESS_SECRET: str = os.getenv("TWITTER_ACCESS_SECRET")
+    SOLANA_RPC_URL: str = os.getenv("SOLANA_RPC_URL")
+    ETHEREUM_RPC_URL: str = os.getenv("ETHEREUM_RPC_URL")
+    MORALIS_API_KEY: str = os.getenv("MORALIS_API_KEY")
 
 settings = Settings()
