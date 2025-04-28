3befbd821b0c1a4599eba90daa42e7d39feab082
max10788 <98173596+max10788@users.noreply.github.com>
2025-04-28 16:44:22 +0200

    Update endpoints.py

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, BackgroundTasks
 from pydantic import BaseModel, constr, conint
 from sqlalchemy.orm import Session
 from datetime import datetime
