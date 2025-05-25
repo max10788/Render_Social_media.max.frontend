48b2d03d06df515350c4e22c34043f28818042f9
max10788 <98173596+max10788@users.noreply.github.com>
2025-05-25 11:33:00 +0200

    Create cache.py

---
+++ app/api/cache.py
@@ -0,0 +1,13 @@
+# app/core/cache.py
+
+from typing import Dict, Any
+
+class InMemoryCache:
+    def __init__(self):
+        self.storage: Dict[str, Any] = {}
+
+    def get(self, key: str) -> Optional[Dict]:
+        return self.storage.get(key)
+
+    def set(self, key: str, value: Dict, ttl: int = 300):
+        self.storage[key] = value
