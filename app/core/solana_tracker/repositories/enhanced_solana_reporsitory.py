ea2a023e40781e066cf31becc10a5e641bcc1eb2
max10788 <98173596+max10788@users.noreply.github.com>
2025-06-16 11:48:39 +0200

    Update enhanced_solana_reporsitory.py

--- app/core/solana_tracker/repositories/enhanced_solana_reporsitory.py
+++ app/core/solana_tracker/repositories/enhanced_solana_reporsitory.py
@@ -5,7 +5,7 @@ import aiohttp
 from datetime import datetime
 from decimal import Decimal
 
-from app.core.solana_tracker.repositories import SolanaRepository
+from app.core.solana_tracker.repositories.solana_repository import SolanaRepository
 from app.core.solana_tracker.repositories.utils.rpc_endpoint_manager import RpcEndpointManager
 from app.core.solana_tracker.repositories.utils.enhanced_retry_utils import (
     enhanced_retry_with_backoff,
