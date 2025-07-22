e7217a760e18a5b3f0995a9ea89d1bc5e274d3d5
max10788 <98173596+max10788@users.noreply.github.com>
2025-07-22 23:30:32 +0200

    Update and rename logger.js to logger.py

---
+++ app/core/backend-crypto-tracker/utils/logger.py
@@ -0,0 +1,10 @@
+# app/utils/logger.py
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    handlers=[logging.StreamHandler()]
+)
+
+logger = logging.getLogger("CryptoTracker")
