45dedfa691d4ae527a190aea37cea695120ac6f1
max10788 <98173596+max10788@users.noreply.github.com>
2025-07-22 23:33:14 +0200

    Update etherscan-api.js

--- app/core/backend-crypto-tracker/services/eth/etherscan-api.js
+++ app/core/backend-crypto-tracker/services/eth/etherscan-api.js
@@ -1,1 +1,16 @@
+# app/services/eth/etherscan_api.py
+import requests
+import os
 
+ETHERSCAN_API_KEY = os.getenv("ETHERSCAN_API_KEY")
+
+def fetch_transaction(hash: str):
+    url = "https://api.etherscan.io/api "
+    params = {
+        "module": "proxy",
+        "action": "eth_getTransactionByHash",
+        "txhash": hash,
+        "apikey": ETHERSCAN_API_KEY
+    }
+    response = requests.get(url, params=params)
+    return response.json().get("result")
