bd436ff031559180a4afac70d2a3108c0977870b
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-01 21:00:51 +0200

    Create App.jsx

---
+++ app/frontend/src/App.jsx
@@ -0,0 +1,14 @@
+// src/App.jsx
+import React from 'react';
+import Dashboard from './pages/Dashboard';
+import './index.css'; // Import Tailwind CSS
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
