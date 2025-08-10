e5ec7c71dd201d659fe17b9180dd27e6dff256de
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-10 22:15:12 +0200

    Update sources.py

--- app/core/option_pricing/data/sources.py
+++ app/core/option_pricing/data/sources.py
@@ -4,6 +4,7 @@ from ..exchanges.base import ExchangeClient
 from ..exchanges.binance import BinanceClient
 from ..exchanges.coinbase import CoinbaseClient
 from ..exchanges.kraken import KrakenClient
+from ..exchanges.bitget import BitgetClient
 from ..utils.exceptions import DataUnavailableError
 import os
 from dotenv import load_dotenv
@@ -19,7 +20,8 @@ class DataSourceManager:
         self.exchange_clients = {
             'binance': None,
             'coinbase': None,
-            'kraken': None
+            'kraken': None,
+            'bitget': None
         }
         
         # API Endpoints aus .env lesen
@@ -28,7 +30,8 @@ class DataSourceManager:
             'solana': os.getenv('SOLANA_API_URL', 'https://api.mainnet-beta.solana.com'),
             'binance': os.getenv('BINANCE_API_URL', 'https://api.binance.com/api/v3'),
             'coinbase': os.getenv('COINBASE_API_URL', 'https://api.coinbase.com/v2'),
-            'kraken': os.getenv('KRAKEN_API_URL', 'https://api.kraken.com/0/public')
+            'kraken': os.getenv('KRAKEN_API_URL', 'https://api.kraken.com/0/public'),
+            'bitget': os.getenv('BITGET_API_URL', 'https://api.bitget.com/api/spot/v1')
         }
     
     def get_current_price(self, symbol: str, blockchain: str, exchanges: Optional[List[str]] = None) -> float:
@@ -80,4 +83,6 @@ class DataSourceManager:
                 self.exchange_clients[exchange] = CoinbaseClient(self.endpoints['coinbase'])
             elif exchange == 'kraken':
                 self.exchange_clients[exchange] = KrakenClient(self.endpoints['kraken'])
+            elif exchange == 'bitget':
+                self.exchange_clients[exchange] = BitgetClient(self.endpoints['bitget'])
         return self.exchange_clients[exchange]
