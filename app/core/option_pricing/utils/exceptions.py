fc0d85fa63010ff1407e4222d932380359427e97
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-10 21:52:03 +0200

    Update exceptions.py

--- app/core/option_pricing/utils/exceptions.py
+++ app/core/option_pricing/utils/exceptions.py
@@ -1,1 +1,19 @@
+class OptionPricingError(Exception):
+    """Basis-Klasse für alle OptionPricing-Fehler"""
+    pass
 
+class PricingError(OptionPricingError):
+    """Fehler bei der Optionspreisberechnung"""
+    pass
+
+class DataUnavailableError(OptionPricingError):
+    """Fehler bei der Datenbeschaffung"""
+    pass
+
+class BlockchainError(OptionPricingError):
+    """Fehler bei der Blockchain-Kommunikation"""
+    pass
+
+class ExchangeError(OptionPricingError):
+    """Fehler bei der Börsenkommunikation"""
+    pass
