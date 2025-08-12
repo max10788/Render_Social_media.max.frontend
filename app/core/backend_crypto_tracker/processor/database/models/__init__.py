f34c161ab3c9101ca209ffdaa06c392068a9fa31
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-12 18:29:41 +0200

    Update __init__.py

--- app/core/backend_crypto_tracker/processor/database/models/__init__.py
+++ app/core/backend_crypto_tracker/processor/database/models/__init__.py
@@ -1,15 +1,12 @@
 # processor/database/models/__init__.py
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, DateTime, func
-
 # Zentrale Base-Klasse für alle Modelle
 Base = declarative_base()
-
 class TimestampMixin:
     """Mixin für Zeitstempel-Felder"""
     created_at = Column(DateTime, default=func.now())
     updated_at = Column(DateTime, default=func.now(), onupdate=func.now())
-
 # Importiere alle Modelle
 from .address import Address
 from .cluster import Cluster
