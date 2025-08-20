d219160ec75405b9e82dd7f048a0ed3a536caeb1
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-20 19:59:57 +0200

    Update next.config.js

--- app/crypto-token-analysis-dashboard/next.config.js
+++ app/crypto-token-analysis-dashboard/next.config.js
@@ -1,13 +1,8 @@
-// app/crypto-token-analysis-dashboard/next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
-  trailingSlash: true,
   images: {
     unoptimized: true
-  },
-  skipTrailingSlashRedirect: true
+  }
 }
 
 module.exports = nextConfig
