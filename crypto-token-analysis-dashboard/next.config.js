faa12eac22ae9215d56f90261952dbe3ba8e70a2
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-25 21:44:05 +0200

    Update next.config.js

--- crypto-token-analysis-dashboard/next.config.js
+++ crypto-token-analysis-dashboard/next.config.js
@@ -2,7 +2,15 @@
 const nextConfig = {
   images: {
     unoptimized: true
-  }
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://render-social-media-max-n89a.onrender.com/api/:path*',
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
