749712e668fd7849d79e1bdde416c760c412253a
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-31 17:12:56 +0200

    Update Dashboard.js

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react'; // useEffect entfernt
 import { apiService } from '../services/api';
 import { useApi } from '../hooks/useApi';
 import '../App.css';
