628b9c12da40721764c2d304e625459f752a92d3
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-31 15:14:40 +0200

    Update App.js

--- scr/App.js
+++ scr/App.js
@@ -1,1 +1,15 @@
+import React from 'react';
 
+function App() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px' }}>
+      <h1>Willkommen bei meiner React App!</h1>
+      <p>Diese App läuft auf Render.com</p>
+      <button onClick={() => alert('Hallo Render!')}>
+        Klick mich!
+      </button>
+    </div>
+  );
+}
+
+export default App;
