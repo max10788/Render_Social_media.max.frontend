3676ee51a85c41795506c3ba5672bf5e1ec6b98c
max10788 <98173596+max10788@users.noreply.github.com>
2025-08-31 16:23:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
-      .then(() => console.log('ServiceWorker registered'));
-  });
