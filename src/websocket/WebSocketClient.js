f82c1072dba57260070e4caf65e513a13b65cc74
max10788 <98173596+max10788@users.noreply.github.com>
2025-09-07 19:07:11 +0200

    Update WebSocketClient.js

--- src/websocket/WebSocketClient.js
+++ src/websocket/WebSocketClient.js
@@ -1,116 +1,96 @@
 class WebSocketClient {
-  constructor(url) {
-    this.url = url;
+  constructor() {
     this.socket = null;
+    this.listeners = {};
     this.reconnectAttempts = 0;
     this.maxReconnectAttempts = 5;
     this.reconnectInterval = 3000;
-    this.subscriptions = new Set();
-    this.eventHandlers = {
-      open: [],
-      close: [],
-      error: [],
-      message: []
+  }
+
+  connect(channel) {
+    const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
+    const wsUrl = `${protocol}//${window.location.host}/ws/${channel}`;
+    
+    this.socket = new WebSocket(wsUrl);
+    
+    this.socket.onopen = () => {
+      console.log('WebSocket connected');
+      this.reconnectAttempts = 0;
+      this.emit('open');
     };
     
-    this.connect();
-  }
-
-  connect() {
-    try {
-      this.socket = new WebSocket(this.url);
-      
-      this.socket.onopen = () => {
-        console.log('WebSocket connected');
-        this.reconnectAttempts = 0;
-        this.emit('open');
-        
-        // Resubscribe to all channels after reconnect
-        this.subscriptions.forEach(channel => {
-          this.subscribe(channel);
-        });
-      };
-      
-      this.socket.onclose = (event) => {
-        console.log('WebSocket disconnected:', event.code, event.reason);
-        this.emit('close', event);
-        
-        // Attempt to reconnect
-        if (this.reconnectAttempts < this.maxReconnectAttempts) {
-          this.reconnectAttempts++;
-          setTimeout(() => this.connect(), this.reconnectInterval);
-        }
-      };
-      
-      this.socket.onerror = (error) => {
-        console.error('WebSocket error:', error);
-        this.emit('error', error);
-      };
-      
-      this.socket.onmessage = (event) => {
-        try {
-          const data = JSON.parse(event.data);
-          this.emit('message', data);
-        } catch (error) {
-          console.error('Error parsing WebSocket message:', error);
-        }
-      };
-    } catch (error) {
-      console.error('Error creating WebSocket connection:', error);
+    this.socket.onmessage = (event) => {
+      try {
+        const data = JSON.parse(event.data);
+        this.emit('message', data);
+      } catch (error) {
+        console.error('Error parsing WebSocket message:', error);
+      }
+    };
+    
+    this.socket.onclose = () => {
+      console.log('WebSocket disconnected');
+      this.emit('close');
+      this.attemptReconnect(channel);
+    };
+    
+    this.socket.onerror = (error) => {
+      console.error('WebSocket error:', error);
       this.emit('error', error);
-    }
+    };
   }
-
-  on(event, handler) {
-    if (this.eventHandlers[event]) {
-      this.eventHandlers[event].push(handler);
-    }
-  }
-
-  off(event, handler) {
-    if (this.eventHandlers[event]) {
-      this.eventHandlers[event] = this.eventHandlers[event].filter(h => h !== handler);
-    }
-  }
-
-  emit(event, data) {
-    if (this.eventHandlers[event]) {
-      this.eventHandlers[event].forEach(handler => handler(data));
-    }
-  }
-
-  send(data) {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      this.socket.send(typeof data === 'string' ? data : JSON.stringify(data));
+  
+  attemptReconnect(channel) {
+    if (this.reconnectAttempts < this.maxReconnectAttempts) {
+      this.reconnectAttempts++;
+      setTimeout(() => {
+        console.log(`Attempting to reconnect... (${this.reconnectAttempts}/${this.maxReconnectAttempts})`);
+        this.connect(channel);
+      }, this.reconnectInterval);
     } else {
-      console.error('WebSocket is not connected');
+      console.error('Max reconnection attempts reached');
     }
   }
-
-  subscribe(channel) {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      this.send({
-        action: 'subscribe',
-        channel
-      });
-      this.subscriptions.add(channel);
-    }
-  }
-
-  unsubscribe(channel) {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      this.send({
-        action: 'unsubscribe',
-        channel
-      });
-      this.subscriptions.delete(channel);
-    }
-  }
-
-  close() {
+  
+  disconnect() {
     if (this.socket) {
       this.socket.close();
     }
+  }
+  
+  send(data) {
+    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
+      this.socket.send(JSON.stringify(data));
+    }
+  }
+  
+  on(event, callback) {
+    if (!this.listeners[event]) {
+      this.listeners[event] = [];
+    }
+    this.listeners[event].push(callback);
+  }
+  
+  emit(event, data) {
+    if (this.listeners[event]) {
+      this.listeners[event].forEach(callback => callback(data));
+    }
+  }
+  
+  onOpen(callback) {
+    this.on('open', callback);
+  }
+  
+  onMessage(callback) {
+    this.on('message', callback);
+  }
+  
+  onClose(callback) {
+    this.on('close', callback);
+  }
+  
+  onError(callback) {
+    this.on('error', callback);
   }
 }
 
