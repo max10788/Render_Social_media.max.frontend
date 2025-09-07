e2eb394bd18d31de90661633e54a61895399dcfa
max10788 <98173596+max10788@users.noreply.github.com>
2025-09-07 19:34:21 +0200

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -4,4 +4,9 @@ const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001', {
   autoConnect: false,
 });
 
+export const initSocket = () => {
+  socket.connect();
+  return socket;
+};
+
 export default socket;
