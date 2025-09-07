956c598353039d9d69e2d31327bcf9f165c256cb
max10788 <98173596+max10788@users.noreply.github.com>
2025-09-07 19:15:47 +0200

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,25 +1,7 @@
-import { io } from 'socket.io-client';
+import io from 'socket.io-client';
 
-let socket;
+const socket = io(process.env.REACT_APP_SOCKET_URL || 'http://localhost:3001', {
+  autoConnect: false,
+});
 
-export const initSocket = () => {
-  // Ersetzen Sie die URL mit Ihrer WebSocket-Server-URL
-  socket = io(process.env.REACT_APP_SOCKET_URL || 'wss://api.example.com');
-  
-  socket.on('connect', () => {
-    console.log('Connected to server');
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Disconnected from server');
-  });
-
-  return socket;
-};
-
-export const getSocket = () => {
-  if (!socket) {
-    return initSocket();
-  }
-  return socket;
-};
+export default socket;
