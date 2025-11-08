7408c1b954dc9f28cd5880bae60e8802001de367
max10788 <98173596+max10788@users.noreply.github.com>
2025-11-08 16:56:48 +0100

    Update MultiCandleSelectionModal.js

--- src/components/ui/MultiCandleSelectionModal.js
+++ src/components/ui/MultiCandleSelectionModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../CustomCandlestickChart.css';
+import './CustomCandlestickChart.css';
 
 const MultiCandleSelectionModal = ({
   selectedCandles = [],
